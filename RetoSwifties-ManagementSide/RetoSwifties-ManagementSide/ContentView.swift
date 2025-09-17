b0f3e7c3fa04af1399507247d4e7a172eb7d3485
4ll3ssandro <a00839934@tec.mx>
2025-09-17 15:48:51 -0600

    .

---
+++ RetoSwifties-ManagementSide/RetoSwifties-ManagementSide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetoSwifties-ManagementSide
+//
+//  Created by Alumno on 17/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetoSwifties-ManagementSide/RetoSwifties-ManagementSide/RetoSwifties_ManagementSideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoSwifties_ManagementSideApp.swift
+//  RetoSwifties-ManagementSide
+//
+//  Created by Alumno on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct RetoSwifties_ManagementSideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
